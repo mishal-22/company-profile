c628863e01b35028caee7d717efa47ff14712c25
Mishal <mishalknd@gmail.com>
2024-03-18 09:50:39 +0530

    items added to the cart and cart items remove function created

--- productsJs.js
+++ productsJs.js
@@ -9,9 +9,9 @@ function loadContent(){
 btn.addEventListener('click',addCart)
   });
 
-if(JSON.parse(localStorage.getItem("products"))!=null){
-cartItems=JSON.parse(localStorage.getItem("products"))
-}
+
+cartItems=JSON.parse(localStorage.getItem("products") || [])
+
 // let itemList=[]
 /** =================================================================
  * @author 
@@ -48,6 +48,9 @@ function addCart(){
 
 function displayItems(){
    cartItems=JSON.parse(localStorage.getItem("products"))
+   if(cartItems.length==0){
+    document.getElementById('order-place').style.display='none';
+   }
   console.log(cartItems)
   var displayDiv=document.getElementById('cart-content')
   if(!displayDiv){
@@ -56,20 +59,51 @@ function displayItems(){
   }
   displayDiv.innerHTML='';
   
-  cartItems.forEach(item=>{
+  cartItems.forEach((item,index)=>{
     const element=document.createElement('div')
-     element.textContent=item.title+'-'+item.price;
+     element.innerHTML=`<div class="card mb-3">
+     <div class="card-body">
+       <div class="d-flex justify-content-between">
+         <div class="d-flex flex-row align-items-center">
+           <div>
+             <img
+               src=${item.imgScr}
+               class="img-fluid rounded-3" alt="Shopping item" style="width: 65px;">
+           </div>
+           <div class="ms-3">
+             <h5>${item.title}</h5>
+             
+           </div>
+         </div>
+         <div class="d-flex flex-row align-items-center">
+           <div style="width: 50px;">
+             <h5 class="fw-normal mb-0">X 1</h5>
+           </div>
+           <div style="width: 150px;">
+             <h5 class="mb-0">${item.price}</h5>
+           </div>
+           <button class="cart-remove-btn" onclick="removeCartProduct(${index})"><i class="bi bi-trash"></i></button>
+          
+         </div>
+       </div>
+     </div>
+   </div>`;
      displayDiv.appendChild(element)
   });
+  grandTotal();
 
 }
 function productCount(){
   
   var countDisplay=document.getElementById('prdctCnt')
+  let count=cartItems.length;
   if(count!=0){
-    let count=cartItems.length;
+    
      countDisplay.style.display="block"
      countDisplay.innerHTML=count
+  }
+  else{
+    countDisplay.style.display="none"
   }
   console.log(count)
 }
@@ -77,7 +111,31 @@ function productCount(){
 window.onload=loadFunctions;
 
 function loadFunctions(){
-  // localStorage.clear();
+ 
   displayItems()
   productCount()
+  grandTotal()
+}
+function removeCartProduct(id){
+cartItems.splice(id,1)
+localStorage.setItem("products",JSON.stringify(cartItems))
+loadFunctions()
+// console.log(id)
+}
+
+function grandTotal(){
+  let total=0;
+  cartItems.forEach((item)=>{
+   total+=parseInt(item.price.replace(/\D/g,''));
+  })
+  document.getElementById('total-price').innerHTML=total;
+}
+function placeTheOrder(){
+  alert("Order is placed!")
+  // localStorage.removeItem()
+  cartItems.forEach((item,id)=>{
+    cartItems.splice(id)
+    localStorage.setItem("products",JSON.stringify(cartItems))
+    loadFunctions()
+  })
 }
